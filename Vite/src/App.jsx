a6f7ec094be92779f70a5a4d62ae2b7849f98560
shashanklko <shashankkm2003@gmail.com>
2026-04-15 02:17:59 +0530

    new practise

--- Vite/src/App.jsx
+++ Vite/src/App.jsx
@@ -1,121 +1,64 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
-import './App.css'
+// Filename - App.js
+
+import React from "react";
+import axios from "axios";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const fetchData = () => {
+    //Connect to the given URL and get the data in the data JSON object, pass the data JSON object as a parameter to the .then block
+    axios.get("https://jsonplaceholder.typicode.com/posts")
+      .then(data => {
+        //Get the index entered by the user
+        var idx = document.getElementById("idx").value;
+
+        //Set the border of the existing table from 0px to 1pxhhhhh
+        document.getElementById("result").border = "1px";
+
+        //Put the values of the header in the table
+        document.getElementById("fh").innerHTML = "ID";
+        document.getElementById("sh").innerHTML = "TITLE";
+
+        //Put the values into the cells in the first row
+        document.getElementById("col1").innerHTML = data.data[idx - 1].id;
+        document.getElementById("col2").innerHTML = data.data[idx - 1].title;
+      })
+      .catch(error => console.log(error));
+
+
+  };
+
+
+
 
   return (
-    <>
-      <section id="center">
-        <div className="hero">
-          <img src={heroImg} className="base" width="170" height="179" alt="" />
-          <img src={reactLogo} className="framework" alt="React logo" />
-          <img src={viteLogo} className="vite" alt="Vite logo" />
-        </div>
-        <div>
-          <h1>Get started</h1>
-          <p>
-            Edit <code>src/App.jsx</code> and save to test <code>HMR</code>
-          </p>
-        </div>
-        <button
-          className="counter"
-          onClick={() => setCount((count) => count + 1)}
-        >
-          Count is {count}
-        </button>
-      </section>
-
-      <div className="ticks"></div>
-
-      <section id="next-steps">
-        <div id="docs">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#documentation-icon"></use>
-          </svg>
-          <h2>Documentation</h2>
-          <p>Your questions, answered</p>
-          <ul>
-            <li>
-              <a href="https://vite.dev/" target="_blank">
-                <img className="logo" src={viteLogo} alt="" />
-                Explore Vite
-              </a>
-            </li>
-            <li>
-              <a href="https://react.dev/" target="_blank">
-                <img className="button-icon" src={reactLogo} alt="" />
-                Learn more
-              </a>
-            </li>
-          </ul>
-        </div>
-        <div id="social">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#social-icon"></use>
-          </svg>
-          <h2>Connect with us</h2>
-          <p>Join the Vite community</p>
-          <ul>
-            <li>
-              <a href="https://github.com/vitejs/vite" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#github-icon"></use>
-                </svg>
-                GitHub
-              </a>
-            </li>
-            <li>
-              <a href="https://chat.vite.dev/" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#discord-icon"></use>
-                </svg>
-                Discord
-              </a>
-            </li>
-            <li>
-              <a href="https://x.com/vite_js" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#x-icon"></use>
-                </svg>
-                X.com
-              </a>
-            </li>
-            <li>
-              <a href="https://bsky.app/profile/vite.dev" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#bluesky-icon"></use>
-                </svg>
-                Bluesky
-              </a>
-            </li>
-          </ul>
-        </div>
-      </section>
-
-      <div className="ticks"></div>
-      <section id="spacer"></section>
-    </>
-  )
+    <div className="App" align="center">
+      <input type="text" name="idx" id="idx" placeholder="Enter the index to search" />
+      <button onClick={fetchData}>
+        Fetch Data
+      </button>
+      <br /><br />
+      <table id="result">
+        <thead><tr><th id="fh"></th><th id="sh"></th></tr></thead><tbody><tr><td id="col1"></td><td id="col2"></td></tr></tbody>
+      </table>
+    </div>
+  );
 }
 
-export default App
+export default App;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
